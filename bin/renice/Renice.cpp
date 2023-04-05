18e019b21b2ea9319404b67f2afe72ee9f5c9218
Allan Chan <allanchan@cpp.edu>
2023-04-05 15:43:38 -0700

    Added files to set up renice command

---
+++ bin/renice/Renice.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include "Renice.h"
+#include "sys/renice.h"
+
+Renice::Renice(int argc, char **argv)
+    : POSIXApplication(argc, argv)
+{
+    parser().setDescription("Alters the scheduling priority of a running");
+    parser().registerPositional("PRIORITY", "Priority level to be used");
+    parser().registerPositional("PROCESS_ID", "Process to have its scheduling priority changed");
+    parser().registerFlag('n', "priority", "Specify the scheduling priority to be used for the process");
+}
+
+Renice::~Renice()
+{
+}
+
+Renice::Result Renice::exec()
+{
+    // to be added
+}
---
+++ lib/libposix/sys/renice.h
@@ -0,0 +1,24 @@
+#ifndef __LIBPOSIX_RENICE_H
+#define __LIBPOSIX_RENICE_H
+
+#include <Macros.h>
+#include "types.h"
+
+/**
+ * @brief Change the scheduling priority of a running process.
+ *
+ * @param pid Process ID of child to change priority.
+ * @param priority Priority level to set process to.
+ * @param stat_loc Points to an integer for storing the exit status.
+ * @param options Optional flags.
+ *
+ * @return Process ID of the child on success or -1 on error
+ */
+extern C pid_t renicepid(pid_t pid, int *stat_loc, int options);
+
+/**
+ * @}
+ * @}
+ */
+
+#endif /* __LIBPOSIX_RENICE_H */
