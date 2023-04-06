e75c7034aaebe1f4478e6b8a9d587a66b8ffa8b0
andrew001347 <84065006+andrew001347@users.noreply.github.com>
2023-04-06 10:13:20 -0700

    update

--- bin/ps/ProcessList.cpp
+++ bin/ps/ProcessList.cpp
@@ -56,3 +56,4 @@ ProcessList::Result ProcessList::exec()
 
     write(1, *out, out.length());
     return Success;
+
--- bin/renice/Renice.cpp
+++ bin/renice/Renice.cpp
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <unistd.h>
 #include "Renice.h"
 #include "sys/renice.h"
-#include <ProcessClient.h>
 
 Renice::Renice(int argc, char **argv)
     : POSIXApplication(argc, argv)
@@ -18,35 +17,10 @@ Renice::Renice(int argc, char **argv)
 
 Renice::~Renice()
 {
+    
 }
 
 Renice::Result Renice::exec()
 {
-    if (arguments().get("priority")) {
-        const ProcessClient process;
-        ProcessID pid = (atoi(arguments().get("PROCESS_ID")));
-        int priority = (atoi(arguments().get("PRIORITY")));
-
-        ProcessClient::Info info;
-        const ProcessClient::Result result = process.processInfo(pid, info);
-
-        // Check if the process exists    
-        if (result != ProcessClient::Success) {
-            ERROR("Process of ID '" << pid << "' not found")
-            return InvalidArgument;
-        }
-
-        // Check if the new priority is valid
-        if (priority < 1 || priority > 5) {
-            ERROR("Unable to set priority for process " << pid)
-            return InvalidArgument;
-        }
-
-        renicepid(pid, priority, 0, 0); // Set new priority to given process
-
-        // Output
-        printf("Process %d set to priority %d, from priority %d\n", pid, priority, info.kernelState.priority);
-    }
-
-    return Success;
+    // to be added
 }
