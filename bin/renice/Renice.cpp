9950480b3db9b50a9167a54b5bdbd5b4ba47b9eb
andrew001347 <84065006+andrew001347@users.noreply.github.com>
2023-04-06 22:11:03 -0700

    Update Renice.cpp

--- bin/renice/Renice.cpp
+++ bin/renice/Renice.cpp
@@ -8,7 +8,28 @@
 #include <FreeNOS/User.h>
 #include "sys/types.h"
 
-pid_t renicepid(pid_t pid, int priority, int *stat_loc, int options); // Prototype
+pid_t renicepid(pid_t pid, int priority, int *stat_loc, int options)
+{
+    const ulong result = (ulong) ProcessCtl(pid, RenicePID, priority);
+
+    switch ((const API::Result) (result & 0xffff))
+    {
+        case API::NotFound:
+            errno = ESRCH;
+            return (pid_t) -1;
+
+        case API::Success:
+            if (stat_loc)
+            {
+                *stat_loc = result >> 16;
+            }
+            return pid;
+
+        default:
+            errno = EIO;
+            return (pid_t) -1;
+    }
+}
 
 Renice::Renice(int argc, char **argv)
     : POSIXApplication(argc, argv)
@@ -54,25 +75,4 @@ Renice::Result Renice::exec()
     return Success;
 }
 
-pid_t renicepid(pid_t pid, int priority, int *stat_loc, int options) // Definition
-{
-    const ulong result = (ulong) ProcessCtl(pid, RenicePID, priority);
 
-    switch ((const API::Result) (result & 0xffff))
-    {
-        case API::NotFound:
-            errno = ESRCH;
-            return (pid_t) -1;
-
-        case API::Success:
-            if (stat_loc)
-            {
-                *stat_loc = result >> 16;
-            }
-            return pid;
-
-        default:
-            errno = EIO;
-            return (pid_t) -1;
-    }
-}
