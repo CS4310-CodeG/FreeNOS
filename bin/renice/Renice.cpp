a9322cbaed71c58c050fac56781c20fcee9099cb
Allan Chan <allanchan@cpp.edu>
2023-04-06 18:11:23 -0700

    Added renicepid function

--- bin/renice/Renice.cpp
+++ bin/renice/Renice.cpp
@@ -4,8 +4,12 @@
 #include <errno.h>
 #include <unistd.h>
 #include "Renice.h"
-#include "sys/renice.h"
+//#include "sys/renice.h"
 #include <ProcessClient.h>
+#include <FreeNOS/User.h>
+#include "sys/types.h"
+
+pid_t renicepid(pid_t pid, int priority, int *stat_loc, int options); // Prototype
 
 Renice::Renice(int argc, char **argv)
     : POSIXApplication(argc, argv)
@@ -49,4 +53,27 @@ Renice::Result Renice::exec()
     }
 
     return Success;
+}
+
+pid_t renicepid(pid_t pid, int priority, int *stat_loc, int options) // Definition
+{
+    const ulong result = (ulong) ProcessCtl(pid, RenicePID, priority);
+
+    switch ((const API::Result) (result & 0xffff))
+    {
+        case API::NotFound:
+            errno = ESRCH;
+            return (pid_t) -1;
+
+        case API::Success:
+            if (stat_loc)
+            {
+                *stat_loc = result >> 16;
+            }
+            return pid;
+
+        default:
+            errno = EIO;
+            return (pid_t) -1;
+    }
 }
